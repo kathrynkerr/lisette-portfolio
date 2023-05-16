254508843a42d5dac107b45e75b98ac8ec0d1c91
Kathryn <57072945+kathrynkerr@users.noreply.github.com>
2023-05-15 22:54:08 -0500

    Update audio-player.js

--- audio-player.js
+++ audio-player.js
@@ -92,3 +92,99 @@ function getTimeCodeFromNum(num) {
     seconds % 60
   ).padStart(2, 0)}`;
 }
+
+
+  // Possible improvements:
+// - Change timeline and volume slider into input sliders, reskinned
+// - Change into Vue or React component
+// - Be able to grab a custom title instead of "Music Song"
+// - Hover over sliders to see preview of timestamp/volume change
+
+const audioPlayer2 = document.querySelector(".audio-player2");
+const audio2 = new Audio2(
+  "https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/backsound.mp3"
+);
+//credit for song: Adrian kreativaweb@gmail.com
+
+console.dir(audio2);
+
+audio2.addEventListener(
+  "loadeddata",
+  () => {
+    audioPlayer2.querySelector(".time .length").textContent = getTimeCodeFromNum(
+      audio2.duration
+    );
+    audio2.volume = .75;
+  },
+  false
+);
+
+//click on timeline to skip around
+const timeline = audioPlayer2.querySelector(".timeline");
+timeline.addEventListener("click", e => {
+  const timelineWidth = window.getComputedStyle(timeline).width;
+  const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio2.duration;
+  audio2.currentTime = timeToSeek;
+}, false);
+
+//click volume slider to change volume
+const volumeSlider = audioPlayer2.querySelector(".controls .volume-slider");
+volumeSlider.addEventListener('click', e => {
+  const sliderWidth = window.getComputedStyle(volumeSlider).width;
+  const newVolume = e.offsetX / parseInt(sliderWidth);
+  audio2.volume = newVolume;
+  audioPlayer2.querySelector(".controls .volume-percentage").style.width = newVolume * 100 + '%';
+}, false)
+
+//check audio percentage and update time accordingly
+setInterval(() => {
+  const progressBar = audioPlayer2.querySelector(".progress");
+  progressBar.style.width = audio2.currentTime / audio2.duration * 100 + "%";
+  audioPlayer2.querySelector(".time .current").textContent = getTimeCodeFromNum(
+    audio2.currentTime
+  );
+}, 500);
+
+//toggle between playing and pausing on button click
+const playBtn = audioPlayer2.querySelector(".controls .toggle-play");
+playBtn.addEventListener(
+  "click",
+  () => {
+    if (audio2.paused) {
+      playBtn.classList.remove("play");
+      playBtn.classList.add("pause");
+      audio2.play();
+    } else {
+      playBtn.classList.remove("pause");
+      playBtn.classList.add("play");
+      audio2.pause();
+    }
+  },
+  false
+);
+
+audioPlayer2.querySelector(".volume-button").addEventListener("click", () => {
+  const volumeEl = audioPlayer2.querySelector(".volume-container .volume");
+  audio2.muted = !audio2.muted;
+  if (audio2.muted) {
+    volumeEl.classList.remove("icono-volumeMedium");
+    volumeEl.classList.add("icono-volumeMute");
+  } else {
+    volumeEl.classList.add("icono-volumeMedium");
+    volumeEl.classList.remove("icono-volumeMute");
+  }
+});
+
+//turn 128 seconds into 2:08
+function getTimeCodeFromNum(num) {
+  let seconds = parseInt(num);
+  let minutes = parseInt(seconds / 60);
+  seconds -= minutes * 60;
+  const hours = parseInt(minutes / 60);
+  minutes -= hours * 60;
+
+  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+    seconds % 60
+  ).padStart(2, 0)}`;
+}
